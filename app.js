222cacfdb398e899646d3e6dbc232a67fca092b5
aishwarya <aishwarya.chordia@edu.rptu.de>
2024-03-03 12:44:57 +0100

    Commit 1 backend

---
+++ app.js
@@ -0,0 +1,23 @@
+const express=require('express')
+const cors=require('cors')
+const { db } = require('./db/db')
+const {readdirSync}=require('fs')
+const app=express()
+require('dotenv').config()
+
+const PORT=process.env.PORT
+
+//middlewares
+app.use(express.json())
+app.use(cors())
+
+//registering routes
+readdirSync('./routes').map((route)=>app.use('/api/v1',require('./routes/'+route)))
+
+const server = ()=>{
+    db();
+    app.listen(PORT,()=>{
+        console.log(`Listening on port ${PORT}`)
+    })
+}
+server()
